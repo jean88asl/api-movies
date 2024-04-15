3e650ad030aad460d488932ff27c7854b999c621
Jean Augusto <jean88asl@gmail.com>
2024-04-15 18:44:27 -0300

    added show static images config

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require("express-async-errors")
 const database = require("./database/sqlite")
 
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
 
 const express = require("express")
 
@@ -12,6 +13,10 @@ const app = express()
 
 // indicando que as respostas das requisições serão no formato json
 app.use(express.json())
+
+// exibindo os arquivos estáticos
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+
 // usando as rotas do arquivo index.js que fica na pasta routes
 app.use(routes)
 
